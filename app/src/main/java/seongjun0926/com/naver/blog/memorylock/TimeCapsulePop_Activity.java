f618c70719aebde2d9fd1e0dbea1963ec86396b2
seongjun0926 <seongjun0926@naver.com>
2016-07-19 02:29:58 +0900

    MemoryLock

--- app/src/main/java/seongjun0926/com/naver/blog/memorylock/TimeCapsulePop_Activity.java
+++ app/src/main/java/seongjun0926/com/naver/blog/memorylock/TimeCapsulePop_Activity.java
@@ -2,12 +2,14 @@ package seongjun0926.com.naver.blog.memorylock;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Window;
 
 public class TimeCapsulePop_Activity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.timecapsulepop_activity);
     }
 }
