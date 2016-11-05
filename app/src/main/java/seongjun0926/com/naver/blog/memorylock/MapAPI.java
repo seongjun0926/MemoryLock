953c4a42662a41f642feffd9d775a0914a178813
sonseongjun <seongjun0926@naver.com>
2016-11-05 17:34:56 +0900

    Update MapAPI.java

--- app/src/main/java/seongjun0926/com/naver/blog/memorylock/MapAPI.java
+++ app/src/main/java/seongjun0926/com/naver/blog/memorylock/MapAPI.java
@@ -5,6 +5,6 @@ package seongjun0926.com.naver.blog.memorylock;
  */
 public class MapAPI {
 
-        public static final String MAP_API_KEY_Value = "14f0409b73f10d9a817b7968ca7c9a19";
+        public static final String MAP_API_KEY_Value = "-";
 
 }
